4fd08de5ace147c53aff002f32b57deab90e2d7e
Ayush Dangwal <dhairyad03@gmail.com>
2020-06-12 17:44:00 +0530

    Optimized
    Optimized Script

--- BadUSBKiller.sh
+++ BadUSBKiller.sh
@@ -1,9 +1,10 @@
-lsusb>devices
-while read p; do
-  if [[ "$p" == *"Digispark"* ]]; then
-  	val=$(sed -e 's/Bus \([0-9]\+\) Device \([0-9]\+\).*/\1\/\2/g' <<< $p)
-  fi
-done < devices
-id=$(grep -l $val /sys/bus/usb/devices/*/uevent)
-id1=$(sed -e 's/\/sys\/bus\/usb\/devices\/\(.*\)\/uevent/\1/' <<< $id)
-echo $id1 > /sys/bus/usb/drivers/usb/unbind 
+while true; do
+  while read p; do
+    if [[ "$p" == *"Digispark"* ]]; then
+  	  val=$(sed -e 's/Bus \([0-9]\+\) Device \([0-9]\+\).*/\1\/\2/g' <<< $p)
+  	  id=$(grep -l $val /sys/bus/usb/devices/*/uevent) 2> /dev/null
+      id1=$(sed -e 's/\/sys\/bus\/usb\/devices\/\(.*\)\/uevent/\1/' <<< $id) 2> /dev/null
+      (echo $id1 > /sys/bus/usb/drivers/usb/unbind) 2> /dev/null
+    fi
+  done <<< $(lsusb)
+done
